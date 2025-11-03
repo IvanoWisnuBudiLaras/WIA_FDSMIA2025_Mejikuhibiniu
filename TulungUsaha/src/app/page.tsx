00bfc003b510968dd556d8fee182a642428808b3
IvanoWisnuBudiLaras <ivanowisnubudilaras2008@gmail.com>
2025-11-03 09:34:50 +0700

    Perbaikan

--- TulungUsaha/src/app/page.tsx
+++ TulungUsaha/src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Hero from "@/components/hero";
-import AboutPage from "@/components/about";
+import Hero from "@/components/Hero";
+import AboutPage from "@/components/About";
 import FloatingGallery from "@/components/FloatingGallery";
 
 export default function Page() {
